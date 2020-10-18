c4cd9b53ab2be20a219b3e073e3cda9eddbaf6e1
jstao <t1416249906@163.com>
2020-10-18 21:39:27 +0800

    frist

---
+++ service/config/config.default.js
@@ -0,0 +1,59 @@
+/* eslint valid-jsdoc: "off" */
+
+'use strict';
+
+/**
+ * @param {Egg.EggAppInfo} appInfo app info
+ */
+module.exports = appInfo => {
+  /**
+   * built-in config
+   * @type {Egg.EggAppConfig}
+   **/
+  const config = exports = {};
+
+  // use for cookie sign key, should change to your own and keep security
+  config.keys = appInfo.name + '_1602933621287_478';
+
+  // add your middleware config here
+  config.middleware = [];
+
+  // add your user config here
+  const userConfig = {
+    // myAppName: 'egg',
+  };
+  config.mysql = {
+    // database configuration
+    client: {
+      // host
+      host: 'localhost',
+      // port
+      port: '3306',
+      // username
+      user: 'root',
+      // password
+      password: '12345678',
+      // database
+      database: 'react_blog',    
+    },
+    // load into app, default is open
+    app: true,
+    // load into agent, default is close
+    agent: false,
+  };
+
+  config.security={
+    csrf:{enable:false},
+    domainWhiteList:['*'] //白名单
+  };
+  config.cors = {
+    origin:'http://localhost:3000',
+    credentials: true,//允许Cook跨域
+    allowMethods:'GET,HEAD,PUT,POST,DELETE,PATCH,OPTIONS'
+  }
+
+  return {
+    ...config,
+    ...userConfig,
+  };
+};
